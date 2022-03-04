411319b0ee2202e698561f281ba847c81105151e
Leslie Kellestine <76988803+LeslieKellestine@users.noreply.github.com>
2022-03-04 11:22:23 -0500

    display results from api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results.js"
 import './Dictionary.css';
+
 
 export default function Dictionary () {
     const [keyword, setKeyword] = useState ("");
+    const [results, setResults] = useState (null)
     
     function handleResponse (response) {
         console.log(response.data[0]);
+        setResults (response.data[0]);
     }
 
     function search (event) {
@@ -18,11 +22,14 @@ export default function Dictionary () {
         setKeyword(event.target.value)
     }
     return (
+        <div className="Dictionary">
         <form onSubmit={search}>
             <input 
             type="search"
              autoFocus= {true}
              onChange={handleKeywordChange}/>
         </form>
+        <Results results={results}/>
+        </div>
     );
 }
