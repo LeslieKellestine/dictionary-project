d021e3f1a49be9680e88de48b13746ee50b23981
Leslie Kellestine <76988803+LeslieKellestine@users.noreply.github.com>
2022-03-09 11:51:26 -0500

    inputted photo section

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <main>
           <Dictionary defaultKeyword="Code"/>
         </main>
-        <footer className='App-Footer'>Coded by Leslie Kellestine and is open-sourced on GitHub</footer>
+        <footer className='App-Footer'>Coded by Leslie Kellestine and is <a href="https://github.com/LeslieKellestine/dictionary-project" target="_blank" rel="noreferrer">open-sourced on GitHub</a></footer>
       </div>
     </div>
 
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,30 @@
 import React, { useState } from "react";
 import axios from "axios";
-import Results from "./Results.js"
+import Results from "./Results.js";
+import Photos from "./Photos.js";
 import './Dictionary.css';
 
 
 export default function Dictionary () {
     const [keyword, setKeyword] = useState ("");
-    const [results, setResults] = useState (null)
+    const [results, setResults] = useState (null);
+    const [photos, setPhotos] = useState (null);
     
-    function handleResponse (response) {
+    function handleDictionaryResponse (response) {
         setResults (response.data[0]);
     }
-
+    function handlePexalsResponse (response){
+        setPhotos(response.data.photos);
+    }
     function search (event) {
         event.preventDefault ();
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
+        axios.get(apiUrl).then(handleDictionaryResponse);
+
+        let pexalsApiKey = "563492ad6f91700001000001c105ac0d7b3440{a7a491d4608ccfef36";
+        let pexalsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+        let headers = {Authorization: `Bearer ${pexalsApiKey}`};
+        axios.get(pexalsApiUrl, {headers: headers}).then(handlePexalsResponse);
     }
     function handleKeywordChange (event) {
         setKeyword(event.target.value)
@@ -28,14 +37,14 @@ export default function Dictionary () {
             <input 
             type="search"
              autoFocus= {true}
-             onChange={handleKeywordChange}
-             defaultvalue ={"Type your word"} />
+             onChange={handleKeywordChange} />
         </form>
         <div className="hint">
-            Suggested searches: code, women, tech, cat...
+            Suggested searches: code, tech, computer, app...
         </div>
         </section>
         <Results results={results}/>
+        <Photos photos={photos} />
         </div>
     );
 }
